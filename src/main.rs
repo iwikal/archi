ece47d1780233d621a8c5e9b54f12501f192a08e
iwikal <e.joel.nordstrom@gmail.com>
2018-09-28 21:23:11 +0200

    GL boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,139 @@
 extern crate sdl2;
+extern crate gl;
+
+use std::str;
+use std::mem;
+use std::ptr;
+use std::ffi::CString;
+use gl::types::*;
+
+// Shader sources
+static VS_SRC: &'static str = include_str!("shaders/basicShader.vert");
+static FS_SRC: &'static str = include_str!("shaders/basicShader.frag");
+
+fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+    let shader;
+    unsafe {
+        shader = gl::CreateShader(ty);
+        // Attempt to compile the shader
+        let c_str = CString::new(src.as_bytes()).unwrap();
+        gl::ShaderSource(shader, 1, &c_str.as_ptr(), ptr::null());
+        gl::CompileShader(shader);
+
+        // Get the compile status
+        let mut status = gl::FALSE as GLint;
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len = 0;
+            gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::with_capacity(len as usize);
+            buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+            gl::GetShaderInfoLog(
+                shader,
+                len,
+                ptr::null_mut(),
+                buf.as_mut_ptr() as *mut GLchar,
+            );
+            panic!(
+                "Failed to compile shader:\n{}",
+                str::from_utf8(&buf)
+                    .ok()
+                    .expect("ShaderInfoLog not valid utf8")
+            );
+        }
+    }
+    shader
+}
+
+fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
+    unsafe {
+        let program = gl::CreateProgram();
+        gl::AttachShader(program, vs);
+        gl::AttachShader(program, fs);
+        gl::LinkProgram(program);
+        // Get the link status
+        let mut status = gl::FALSE as GLint;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len: GLint = 0;
+            gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::with_capacity(len as usize);
+            buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+            gl::GetProgramInfoLog(
+                program,
+                len,
+                ptr::null_mut(),
+                buf.as_mut_ptr() as *mut GLchar,
+            );
+            panic!(
+                "Failed to link shader:\n{}",
+                str::from_utf8(&buf)
+                    .ok()
+                    .expect("ProgramInfoLog not valid utf8")
+            );
+        }
+        program
+    }
+}
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_system = sdl_context.video().unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let _window = video_system.window("archi", 800, 600)
+    let window = video_system.window("archi", 800, 600)
+        .opengl()
         .build()
         .unwrap();
+    let _gl_context = window.gl_create_context().unwrap();
+    gl::load_with(|s| video_system.gl_get_proc_address(s) as * const _);
 
+    let vsh = compile_shader(VS_SRC, gl::VERTEX_SHADER);
+    let fsh = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
+    let program = link_program(vsh, fsh);
+
+    let mut vao = 0;
+    let mut vbo = 0;
+    let vertices = [
+        0.0, 1.0,
+        0.5, -0.5,
+        -0.5, -0.5
+    ];
+
+    unsafe {
+        gl::CreateVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            mem::transmute(&vertices[0]),
+            gl::STATIC_DRAW
+        );
+
+        gl::UseProgram(program);
+        gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
+
+        // Specify the layout of the vertex data
+        let pos_attr = gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
+        gl::EnableVertexAttribArray(pos_attr as GLuint);
+        gl::VertexAttribPointer(
+            pos_attr as GLuint,
+            2,
+            gl::FLOAT,
+            gl::FALSE as GLboolean,
+            0,
+            ptr::null(),
+        );
+
+        gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+    }
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
     for e in event_pump.wait_iter() {
         use sdl2::event::Event;
         match e {
@@ -16,7 +141,23 @@ fn main() {
                 println!("Quit");
                 break;
             },
+            Event::KeyDown {keycode, ..} => {
+                match keycode {
+                    Some(k) => {
+                        if k.name() == "Escape" {
+                            println!("<ESC>");
+                            break;
+                        }
+                    },
+                    _ => ()
+                }
+            },
             _ => {}
         }
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+        window.gl_swap_window();
     }
 }
